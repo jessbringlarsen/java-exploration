c860eb55d11e81a1861b02b725fe2d91461d1f1f
jebla <jebla@systematic.com>
2018-07-06 15:03:17 +0200

    initial

---
+++ src/test/java/dk/bringlarsen/exploration/java8/streams/sorting/SortingExplorationTest.java
@@ -0,0 +1,46 @@
+package dk.bringlarsen.exploration.java8.streams.sorting;
+
+import dk.bringlarsen.exploration.java8.model.Person;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Comparator.reverseOrder;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
+
+public class SortingExplorationTest {
+
+    @Test
+    public void testSortAscending() {
+        List<Person> persons = Arrays.asList(
+                Person.create("Person1"),
+                Person.create("Person2"));
+
+        List<String> result = persons.stream()
+                .map(Person::getName)
+                .sorted()
+                .collect(toList());
+
+        assertThat(result.size(), is(2));
+        assertThat(result, contains("Person1", "Person2"));
+    }
+
+    @Test
+    public void testSortDescending() {
+        List<Person> persons = Arrays.asList(
+                Person.create("Person1"),
+                Person.create("Person2"));
+
+        List<String> result = persons.stream()
+                .map(Person::getName)
+                .sorted(reverseOrder())
+                .collect(toList());
+
+        assertThat(result.size(), is(2));
+        assertThat(result, contains("Person2", "Person1"));
+    }
+}
