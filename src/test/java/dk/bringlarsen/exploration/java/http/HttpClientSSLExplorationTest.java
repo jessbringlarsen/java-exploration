892b7c60be5da7a9129003e3f782772d703c918d
jebla <jess@bring-larsen.dk>
2019-08-14 21:17:54 +0200

    Initial

---
+++ src/test/java/dk/bringlarsen/exploration/java/http/HttpClientSSLExplorationTest.java
@@ -0,0 +1,58 @@
+package dk.bringlarsen.exploration.java.http;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import static org.junit.Assert.assertThat;
+
+public class HttpClientSSLExplorationTest {
+
+    private TrustManager[] trustManager = new TrustManager[]{new X509TrustManager(){
+        public X509Certificate[] getAcceptedIssuers(){return null;}
+        public void checkClientTrusted(X509Certificate[] certs, String authType){}
+        public void checkServerTrusted(X509Certificate[] certs, String authType){}
+    }};
+   
+
+    @Test
+    public void simpleGetByPassingSSL() throws Exception {
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustManager, new SecureRandom());
+
+        HttpRequest request = HttpRequest.newBuilder(new URI("https://expired.badssl.com/")).GET().build();
+        HttpClient httpClient = HttpClient.newBuilder().sslContext(sslContext).build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        assertThat(response.body(), CoreMatchers.containsString("expired.badssl.com"));
+    }
+}
