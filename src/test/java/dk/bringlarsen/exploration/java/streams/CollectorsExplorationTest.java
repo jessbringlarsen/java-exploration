cd2dd02a258348e63b6307b23f9dedc4d4e04cbd
Jess Bring-Larsen <jess@bring-larsen.dk>
2021-12-07 22:13:11 +0100

    Test toList

--- src/test/java/dk/bringlarsen/exploration/java/streams/CollectorsExplorationTest.java
+++ src/test/java/dk/bringlarsen/exploration/java/streams/CollectorsExplorationTest.java
@@ -1,5 +1,6 @@
 package dk.bringlarsen.exploration.java.streams;
 
+import dk.bringlarsen.exploration.java.JDK;
 import dk.bringlarsen.exploration.java.model.Person;
 import org.junit.Test;
 
@@ -11,6 +12,7 @@ import java.util.function.Function;
 import static java.util.stream.Collectors.toMap;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class CollectorsExplorationTest {
 
@@ -25,5 +27,17 @@ public class CollectorsExplorationTest {
 
         assertThat(result.get(21).getName(), is("Person1"));
         assertThat(result.get(22).getName(), is("Person2"));
+    }
+
+    @Test
+    @JDK(version = 16, description = "Add Stream.toList Method (JDK-8180352)")
+    public void testStreamToList() {
+        List<String> stringArray = Arrays.asList("1", "2", "3");
+
+        List<Integer> intArray = stringArray.stream()
+                .map(Integer::parseInt)
+                .toList();
+
+        assertArrayEquals(new Integer[] {1, 2, 3}, intArray.toArray());
     }
 }
